35661890c3d4a99ec67de398f3e9a36834864326
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-07-28 13:35:52 +0500

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Question } from "./Types/quiz_types";
 import { QuestionCard } from "./Components/QuestionCard";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import swal from "sweetalert";
 export const App = () => {
   let [quiz, setQuiz] = useState<Question[]>([]);
   let [current, setCurrentState] = useState(0);
@@ -34,10 +35,35 @@ export const App = () => {
   };
   if (showResult) {
     return (
-      <div>
-        <h3>
-          your final score is :{score} out of {quiz.length}
-        </h3>
+      <div
+        style={{
+          width: "300px",
+          height: "300px",
+          backgroundColor: "#fff",
+          boxShadow: "0 4px 8px 0 rgba(7,2,3,5)",
+          padding: " 16px 16px",
+          transition: "0.7s",
+          borderRadius: "5px",
+        }}
+      >
+        {/* {swal({
+          title: "Quiz Score!",
+          text: `your final score is :${score} out of : ${quiz.length}`,
+          icon: "success",
+        })}
+        {quiz} */}
+        <h2
+          style={{
+            marginTop: "5rem",
+            textAlign: "center",
+            justifyContent: "center",
+            display: "flex",
+            color: "red",
+            fontSize: "1.5rem",
+          }}
+        >
+          Your final score is :{score} out of {quiz.length}
+        </h2>
         <button onClick={reStart}>Play again</button>
       </div>
     );
